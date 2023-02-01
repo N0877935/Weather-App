97a8734c9336f734d1620b80978c7ee6a08f7cc1
Joe <joe.finlay@icloud.com>
2023-02-01 10:08:13 +0000

    implemented second API function which returns JSON containing a range of weather data

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
+import './style.css';
 
-
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=a0378840c94dc468a6c0f10315d22343
-// http://api.openweathermap.org/geo/1.0/direct?q=Ipswich&limit=5&appid=a0378840c94dc468a6c0f10315d22343
-// get latitude and longitude from the Geolocation API 
 const input = document.getElementById('search');
 const searchBtn = document.querySelector('.searchBtn');
-let longitude;
-let latitude;
 
-class weatherData {
-    constructor(latitude, longitude) {
-        this.latitude = latitude;
-        this.longitude = longitude;
-    }
+searchBtn.addEventListener('click', getLatandLon);
 
-}
-
- function getLatandLon() {
-    searchBtn.addEventListener('click', async () => {
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=5&appid=a0378840c94dc468a6c0f10315d22343`, { mode: "cors"});
+async function getLatandLon() {
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=5&appid=a0378840c94dc468a6c0f10315d22343`, { mode: "cors"});
         const locationData = await response.json();
-        console.log(locationData);
-        latitude = locationData[0].lat;
-        longitude = locationData[0].lon;
-        return {latitude, longitude};
-    });
+        const latitude = locationData[0].lat;
+        const longitude = locationData[0].lon;
+        getWeatherData(latitude, longitude);
+
+}
+
+async function getWeatherData(lat, lon) {
+    
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a0378840c94dc468a6c0f10315d22343`, {mode: "cors"});
+    const weatherData = await response.json();
+    const name = weatherData.name;
+    const temp = weatherData.main.temp;
+    const feelsLike = weatherData.main.feels_like;
+    createDiv(name, temp, feelsLike);
 }
 
 
+function createDiv(divName, divTemp, divFeels) {
+    const div = document.createElement('div');
+    const h2 = document.createElement('h2');
+    const tempData = document.createElement('p');
+    const feelData = document.createElement('p');
 
-    getLatandLon();
+    div.classList.add('divStyle');
+}
+
+// error handling
