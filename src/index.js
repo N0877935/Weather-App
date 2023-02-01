833829bfa587c353b25963d4f55dbb67f48b9445
Joe <joe.finlay@icloud.com>
2023-02-01 11:57:02 +0000

    added form validation and error handling in async functions

--- src/index.js
+++ src/index.js
@@ -7,25 +7,35 @@ const searchBtn = document.querySelector('.searchBtn');
 searchBtn.addEventListener('click', getLatandLon);
 
 async function getLatandLon() {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=5&appid=a0378840c94dc468a6c0f10315d22343`, { mode: "cors"});
+
+    try {
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${input.value}&limit=5&appid=a0378840c94dc468a6c0f10315d22343`, { mode: "cors"});
         const locationData = await response.json();
         const latitude = locationData[0].lat;
         const longitude = locationData[0].lon;
         getWeatherData(latitude, longitude);
 
+    } catch(err){
+        console.log(err.message);
+        alert("Please enter a valid location");
+    }
+
 }
 
 async function getWeatherData(lat, lon) {
-    
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a0378840c94dc468a6c0f10315d22343`, {mode: "cors"});
-    const weatherData = await response.json();
-    const name = weatherData.name;
-    const description = weatherData.weather[0].description;
-    const temp = weatherData.main.temp;
-    const feelsLike = weatherData.main.feels_like;
-    const humidity = weatherData.main.humidity;
-    console.log(weatherData);
-    createDiv(name, description, temp, feelsLike, humidity);
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a0378840c94dc468a6c0f10315d22343`, {mode: "cors"});
+        const weatherData = await response.json();
+        const name = weatherData.name;
+        const description = weatherData.weather[0].description;
+        const temp = weatherData.main.temp;
+        const feelsLike = weatherData.main.feels_like;
+        const humidity = weatherData.main.humidity;
+        createDiv(name, description, temp, feelsLike, humidity);
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 
@@ -68,4 +78,3 @@ function createDiv(title, divDesc, divTemp, divFeels, divHum) {
     })
 }
 
-// error handling
